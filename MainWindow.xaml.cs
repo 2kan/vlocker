45f1518ccfc530d3f00b38a1eaa9aa24d2208dc2
2kan <dev@2kan.io>
2017-01-30 00:33:23 +1100

    Began work on GUI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,24 +20,58 @@ namespace vlocker
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private Locker m_locker;
+
 		public MainWindow ()
 		{
 			InitializeComponent();
 
 			//LockerFactory.CreateLocker( "locker.vlocker" );
-			Locker locker = new Locker( "locker.vlocker" );
-			locker.LoadLocker();
+			m_locker = new Locker( "locker.vlocker" );
+			m_locker.LoadLocker();
 			//a.FileDirectory.AddFile( "test.txt" );
 			//byte[] file = a.FileDirectory.GetFile( "test.txt" );
 
+			UpdateFileTree();
+		}
+
+		private void UpdateFileTree ()
+		{
 			TreeViewItem treeItem = new TreeViewItem();
 			treeItem.Header = "root";
 
-			string[] filenames = locker.FileDirectory.GetFilenames();
+			string[] filenames = m_locker.FileDirectory.GetFilenames();
 			for ( int i = 0; i < filenames.Length; ++i )
-				treeItem.Items.Add( new TreeViewItem() { Header = filenames[i] } );
+			{
+				TreeViewItem file = new TreeViewItem() { Header = filenames[i] };
+				file.MouseDoubleClick += fileTree_itemDoubleClick;
 
+				treeItem.Items.Add( file );
+			}
+
+			treeItem.IsExpanded = true;
+
+			fileTree.Items.Clear();
 			fileTree.Items.Add( treeItem );
+		}
+
+		private void btnAddFile_Click ( object sender, RoutedEventArgs e )
+		{
+			Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+
+			bool? result = dialog.ShowDialog();
+
+			if ( result == true )
+			{
+				string filename = dialog.FileName;
+				m_locker.FileDirectory.AddFile( filename );
+				UpdateFileTree();
+			}
+		}
+
+		private void fileTree_itemDoubleClick(object sender, RoutedEventArgs e)
+		{
+			output.Text = Encoding.UTF8.GetString(m_locker.FileDirectory.GetFile(((TreeViewItem) sender).Header.ToString()));
 		}
 	}
 }
